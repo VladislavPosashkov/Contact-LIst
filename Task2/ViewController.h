0c20dbe2fdab4fc9c164bc353ff49b6ffa2cca76
Vladislav Posashkov <vladik298@gmail.com>
2015-01-19 17:15:45 +0200

    Initial Commit

---
+++ Task2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Task2
+//
+//  Created by Vladislav Posashkov on 19.01.15.
+//  Copyright (c) 2015 Vladislav Posashkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Task2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Task2
+//
+//  Created by Vladislav Posashkov on 19.01.15.
+//  Copyright (c) 2015 Vladislav Posashkov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
